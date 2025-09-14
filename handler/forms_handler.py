e85363732ce9f08ab3c8c8d91cd9ba9d786b39b8
BrunoXIII-Gav <bruno.gavidia@upch.pe>
2025-09-14 00:12:08 -0500

    refactor: manejo de formularios

--- handler/forms_handler.py
+++ handler/forms_handler.py
@@ -13,6 +13,7 @@ async def handle_preregistro(parsed, form_id, project_id, redis_client):
     if email_from_form:
         if participante_id_long:
             redis_client.setex(f"webhook:email:long:{participante_id_long}", 604800, email_from_form)
+            redis_client.setex(f"webhook:email:form:preRegistro:{participante_id_long}", 604800, email_from_form)
             print(f"📧 Email guardado (long): {participante_id_long} -> {email_from_form}")
         if participante_id_short:
             redis_client.setex(f"webhook:email:short:{participante_id_short}", 604800, email_from_form)
@@ -28,11 +29,14 @@ async def handle_encuesta_principal(parsed, form_id, project_id, redis_client):
         
     short_id = parsed["data"]["preamble"]["entity_details"]["short_id"]
     long_id = parsed["data"]["preamble"]["entity_details"]["long_id"]
+    status = ""
     
     cached_email = None
     if long_id:
         cached_email = redis_client.get(f"webhook:email:long:{long_id}")
         if cached_email:
+            redis_client.delete(f"webhook:email:form:preRegistro:{long_id}")
+            redis_client.setex(f"webhook:email:form:EncNacional:{long_id}",604800, cached_email)
             print(f"Email encontrado en cache (long_id): {cached_email}")
     if not cached_email and short_id:
         cached_email = redis_client.get(f"webhook:email:short:{short_id}")
